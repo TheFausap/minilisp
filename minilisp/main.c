9cfc6f6454deff962198dd528ca6a96783fd1516
Fausto Saporito <fausto.saporito@gmail.com>
2021-09-06 06:46:43 +0200

    Initial Commit

---
+++ minilisp/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  minilisp
+//
+//  Created by Fausto Saporito on 06/09/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
